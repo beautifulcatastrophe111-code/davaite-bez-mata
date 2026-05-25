4c847f304f3fc77964936fdbec29bc18f2f52e1d
beautifulcatastrophe111-code <beautifulcatastrophe111@gmail.com>
2026-05-25 23:19:42 +0300

    Adjust card form logic: slug from nickname and auto cardType by rating

--- components/AdminCardForm.tsx
+++ components/AdminCardForm.tsx
@@ -2,12 +2,11 @@ import StatInput from './StatInput';
 export default function AdminCardForm(){return <>
 <input name='realName' placeholder='Real name' className='text-black p-2 rounded' required/>
 <input name='nickname' placeholder='Nickname' className='text-black p-2 rounded' required/>
-<input name='slug' placeholder='slug' className='text-black p-2 rounded' required/>
 <input name='country' placeholder='Country (e.g. Ukraine)' className='text-black p-2 rounded' />
 <input name='photoUrl' placeholder='Avatar image URL' className='text-black p-2 rounded' />
 <input name='rating' type='number' defaultValue={50} min={1} max={99} className='text-black p-2 rounded' />
 <select name='role' className='text-black p-2 rounded'><option>CARRY</option><option>MID</option><option>OFFLANE</option><option>SUPPORT</option><option>HARD_SUPPORT</option><option>UNIVERSAL</option></select>
-<select name='cardType' className='text-black p-2 rounded'><option>COMMON</option><option>RARE</option><option>EPIC</option><option>LEGENDARY</option><option>IMMORTAL</option></select>
-<StatInput name='skillLaning' label='Lane'/><StatInput name='skillMicro' label='Micro'/><StatInput name='skillMacro' label='Macro'/><StatInput name='skillFarming' label='Itembuilds'/><StatInput name='skillHeroPool' label='Pool'/><StatInput name='skillMental' label='Mentality'/>
+<p className='text-xs text-slate-300'>Card type is assigned automatically from rating.</p>
+<StatInput name='skillLaning' label='Lane'/><StatInput name='skillMicro' label='Micro'/><StatInput name='skillMacro' label='Macro'/><StatInput name='skillFighting' label='Fighting'/><StatInput name='skillHeroPool' label='Pool'/><StatInput name='skillMental' label='Mentality'/>
 <input type='hidden' name='isActive' value='true'/>
 </>}
