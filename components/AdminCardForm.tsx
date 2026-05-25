4b6e9ed932702e487d7c7f96c4c5c5e4e08dbc3f
beautifulcatastrophe111-code <beautifulcatastrophe111@gmail.com>
2026-05-26 01:53:08 +0300

    Restore six-stat admin form while preserving edit defaults

--- components/AdminCardForm.tsx
+++ components/AdminCardForm.tsx
@@ -24,14 +24,19 @@ export default function AdminCardForm({ card }: AdminCardFormProps) {
       />
       <input
         name="slug"
-        placeholder="slug"
+        placeholder="slug, optional"
         defaultValue={card?.slug || ''}
         className="text-black p-2 rounded"
-        required
+      />
+      <input
+        name="country"
+        placeholder="Country (e.g. Ukraine)"
+        defaultValue={card?.country || ''}
+        className="text-black p-2 rounded"
       />
       <input
         name="photoUrl"
-        placeholder="Photo URL / avatar URL"
+        placeholder="Avatar image URL"
         defaultValue={card?.photoUrl || ''}
         className="text-black p-2 rounded"
       />
@@ -51,23 +56,18 @@ export default function AdminCardForm({ card }: AdminCardFormProps) {
         <option>HARD_SUPPORT</option>
         <option>UNIVERSAL</option>
       </select>
-      <select name="cardType" defaultValue={card?.cardType || 'COMMON'} className="text-black p-2 rounded">
-        <option>COMMON</option>
-        <option>RARE</option>
-        <option>EPIC</option>
-        <option>LEGENDARY</option>
-        <option>IMMORTAL</option>
-      </select>
+      <p className="text-xs text-slate-300">Card type is assigned automatically from rating.</p>
 
-      <StatInput name="skillLaning" label="Laning" defaultValue={card?.skillLaning ?? 50} />
-      <StatInput name="skillFarming" label="Farming" defaultValue={card?.skillFarming ?? 50} />
-      <StatInput name="skillMacro" label="Macro" defaultValue={card?.skillMacro ?? 50} />
+      <StatInput name="skillLaning" label="Lane" defaultValue={card?.skillLaning ?? 50} />
       <StatInput name="skillMicro" label="Micro" defaultValue={card?.skillMicro ?? 50} />
-      <StatInput name="skillHeroPool" label="Hero Pool" defaultValue={card?.skillHeroPool ?? 50} />
+      <StatInput name="skillMacro" label="Macro" defaultValue={card?.skillMacro ?? 50} />
+      <StatInput name="skillFarming" label="Itembuilding" defaultValue={card?.skillFarming ?? 50} />
+      <StatInput name="skillHeroPool" label="Pool" defaultValue={card?.skillHeroPool ?? 50} />
+      <StatInput name="skillMental" label="Mentality" defaultValue={card?.skillMental ?? 50} />
 
       <input type="hidden" name="skillFighting" value={card?.skillFighting ?? 50} />
-      <input type="hidden" name="skillMental" value={card?.skillMental ?? 50} />
       <input type="hidden" name="skillComms" value={card?.skillComms ?? 50} />
+      <input type="hidden" name="cardType" value={card?.cardType || 'COMMON'} />
       <input type="hidden" name="isActive" value={String(card?.isActive ?? true)} />
     </>
   );
